d7d529c4caa26eb4591a81c576f9624a28e4c940
Brayan Almengor <brayanalmengor300@gmail.com>
2024-08-03 14:41:26 -0500

    Update Class Main

--- Sprint Boot Practice/1- App ZonaFit (CRUD)/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ Sprint Boot Practice/1- App ZonaFit (CRUD)/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,14 +1,31 @@
 package gm.zona_fit;
 
+import gm.zona_fit.servicio.IClienteServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZonaFitApplication {
+public class ZonaFitApplication implements CommandLineRunner {
+
+	@Autowired
+	private IClienteServicio clienteServicio;
+
+	private static final Logger logger =
+			LoggerFactory.getLogger(ZonaFitApplication.class);
 
 	public static void main(String[] args) {
-
+		logger.info("Iniciando la aplicacion");
+		// Levantar la fabrica de spring
 		SpringApplication.run(ZonaFitApplication.class, args);
+		logger.info("Aplicacion finalizada!");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("*** Aplicacion Zona Fit (GYM) ***");
+	}
 }
