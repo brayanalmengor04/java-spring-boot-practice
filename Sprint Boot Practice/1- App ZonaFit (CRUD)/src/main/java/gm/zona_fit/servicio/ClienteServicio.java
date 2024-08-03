b6a971b0f866cdf04361c4173f5466bb7f1c6f5b
Brayan Antonio <112229331+brayanalmengor04@users.noreply.github.com>
2024-08-02 20:22:26 -0500

    Inyeccion de dependencias

--- Sprint Boot Practice/1- App ZonaFit (CRUD)/src/main/java/gm/zona_fit/servicio/ClienteServicio.java
+++ Sprint Boot Practice/1- App ZonaFit (CRUD)/src/main/java/gm/zona_fit/servicio/ClienteServicio.java
@@ -6,31 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-@Service
+@Service // Anotar como la anotacion servicio de Spring 
 public class ClienteServicio implements IClienteServicio{
 
-    @Autowired
+    @Autowired   //Inyectar dependencia ClienteRepositorio (Todos los metodos)
     private ClienteRepositorio clienteRepositorio;
 
     @Override
     public List<Cliente> listarClientes() {
-        List<Cliente> clientes = clienteRepositorio.findAll();
+        List<Cliente> clientes = clienteRepositorio.findAll(); // Ejemplo implementacion para listar todos los clientes
         return clientes;
     }
 
     @Override
     public Cliente buscarClientePorId(Integer idCliente) {
-        Cliente cliente = clienteRepositorio.findById(idCliente).orElse(null);
+        Cliente cliente = clienteRepositorio.findById(idCliente).orElse(null);  // Ejemplo implementacion buscar por id 
         return cliente;
     }
 
     @Override
     public void guardarCliente(Cliente cliente) {
-        clienteRepositorio.save(cliente);
+        clienteRepositorio.save(cliente); // Ejemplo implementacion guardar un cliente 
     }
 
     @Override
     public void eliminarCliente(Cliente cliente) {
-        clienteRepositorio.delete(cliente);
+        clienteRepositorio.delete(cliente); // Ejemplo eliminar un cliente .
     }
 }
