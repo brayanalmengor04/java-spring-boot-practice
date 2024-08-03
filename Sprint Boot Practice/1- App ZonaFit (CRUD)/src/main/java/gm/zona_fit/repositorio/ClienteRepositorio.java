aa52c9d97db1c175286a9381c301e89897634eb0
Brayan Antonio <112229331+brayanalmengor04@users.noreply.github.com>
2024-08-02 20:19:37 -0500

    update cliente repository

--- Sprint Boot Practice/1- App ZonaFit (CRUD)/src/main/java/gm/zona_fit/repositorio/ClienteRepositorio.java
+++ Sprint Boot Practice/1- App ZonaFit (CRUD)/src/main/java/gm/zona_fit/repositorio/ClienteRepositorio.java
@@ -2,6 +2,6 @@ package gm.zona_fit.repositorio;
 
 import gm.zona_fit.modelo.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// Esta Extiende de otra interfas que es la que tiene todos los metodos como (ListarTodo , Lista por ID , Guardar , Eliminar...)
 public interface ClienteRepositorio extends JpaRepository<Cliente,Integer> {
 }
