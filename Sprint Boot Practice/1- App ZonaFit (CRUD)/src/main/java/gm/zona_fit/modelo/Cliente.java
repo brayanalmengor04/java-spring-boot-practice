3f2bf172b33e255019fadcf754c48241cc446689
Brayan Antonio <112229331+brayanalmengor04@users.noreply.github.com>
2024-08-02 19:41:00 -0500

    update  custommer

--- Sprint Boot Practice/1- App ZonaFit (CRUD)/src/main/java/gm/zona_fit/modelo/Cliente.java
+++ Sprint Boot Practice/1- App ZonaFit (CRUD)/src/main/java/gm/zona_fit/modelo/Cliente.java
@@ -6,15 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@EqualsAndHashCode
-public class Cliente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@Entity // Capa Entity anotacion la clase es una entidad
+@Data   // Esto simplifica la generacion de Getters and Setters            LOMBOK
+@NoArgsConstructor // Un constructor vacio sin argumentos      
+@AllArgsConstructor // Un Constructor con todos los argumentos    
+@ToString   // Metodo ToString  
+@EqualsAndHashCode   // Comparar Equals y Hascode
+public class Cliente { 
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Asignacion de estrategia Generaion tipo Identy 
     private Integer id;
     private String nombre;
     private String apellido;
